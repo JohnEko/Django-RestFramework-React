2bbbb430c700d409a4a673ac91c648d9d5f01e61
efezino10@gmail.com <efezino10@gmail.com>
2025-02-17 17:05:45 +0000

    Fixed model issues

--- amebo/api/serializers.py
+++ amebo/api/serializers.py
@@ -1,36 +1,21 @@
-# from rest_framework import serializers
-# from .models import Post, Category, Comment
+from rest_framework import serializers
+from .models import Post, Category, Comment
 
 
-# class CategorySerializer(serializers.ModelSerializer):
-#     class Meta:
-#         model = Category
-#         fields = '__all__'
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = '__all__'
 
-# class PostSerializer(serializers.ModelSerializer):
-#     class Meta:
-#         model = Post
-#         fields = ('id', 
-#                   'title', 
-#                   'author',
-#                   'content', 
-#                   'participants',
-#                   'image', 
-#                   'created_at', 
-#                   'updated_at', 
-#                   'category', 
-#                   'views')
+class PostSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Post
+        fields = '__all__'
+        # ('id', 'title', 'author','content', 'participants','image', 'created_at', 'updated_at', 'category', 'views')
 
 
-#         def create(self, valid_data):
-#             category = valid_data.pop("category", [])
-#             post = Post.objects.create(**valid_data)
-#             for categories in category:
-#                 Post.category.create(categories)
-#                 return post
-
-# class CommentSerializer(serializers.ModelSerializer):
-#     class Meta:
-#         model = Comment
-#         fields = '__all__'
-#         # filds = ["id", "title", "content", "published_date"]
+class CommentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Comment
+        fields = '__all__'
+        # filds = ["id", "title", "content", "published_date"]
